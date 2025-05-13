f2c26bfe6ac557a301c3498d02cd1a5366e75368
Hüsamettin ARABACI <harabaci88@gmail.com>
2025-05-13 17:19:16 +0000

    test(core): add basic test to enable tarpaulin coverage

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
     println!("🚀 hexaFn is ready. Welcome to the 6F lifecycle engine!");
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
